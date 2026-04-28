8d112df3dadba76061d0b2ae4be199fa73c875d7
Vaibhav <vaibhavlokhande1798@gmail.com>
2026-04-28 17:20:18 +0530

    CI run-3

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -76,8 +76,8 @@ def main():
         # test_size = 0.2
         
         #df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
-        # s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
-        # df = s3.fetch_file_from_s3("data.csv")
+        s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
+        df = s3.fetch_file_from_s3("data.csv")
 
 
 
